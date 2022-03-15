a89223d72536720afed19a65f63403346aa2ad5a
Bishnu <bishnuthapako@gmail.com>
2022-03-15 18:46:30 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Write your solution here!
 
 const cats = ["Milo", "Otis", "Garfield"];
+
+
 function destructivelyAppendCat(name){
     return name =  cats.push("Ralph")
 }
@@ -8,26 +10,33 @@ function destructivelyPrependCat(name){
     return name = cats.unshift("Bob")  //prepends a cat to the beginning of the cats array
 }
 function destructivelyRemoveLastCat(){
-   return cats.pop("Garfield")  //removes the last cat from the cats array
-}
-function destructivelyRemoveFirstCat(){//removes the first cat from the cats array
-    return cats.shift("Milo")
+   return cats.pop()  //removes the last cat from the cats array ("Garfield")
 }
 
-function appendCat(name){//appends a cat to the cats array and returns a new array, leaving the cats array unchanged
-    name = [...cats, "Broom"]
-    return name
+function destructivelyRemoveFirstCat(){//removes the first cat from the cats array ("Milo")
+    return cats.shift()
+}  
+function appendCat(name){
+    const cats2 = [...cats, name];
+    return cats2
 }
-  function prependCat(name){ // prepends a cat to the cats array and returns a new array, leaving the cats array unchanged
-    name = ["Arnold", ...cats]
-    return name
-}
+console.log(appendCat("Broom"))
 
- function removeLastCat(){// removes the last cat in the cats array and returns a new array, leaving the cats array unchanged
-    return cats.slice(0,2)
-}
 
-function  removeFirstCat(){//removes the first cat from the cats array and returns a new array, leaving the cats array unchanged
-    return cats.slice(1)
+function prependCat(name){ 
+    const catsArnold = [name, ...cats]
+    return catsArnold
 }
+console.log(prependCat('Arnold'));
 
+ function removeLastCat(){
+    const catRemoveLast = cats.slice(0,2)
+    return catRemoveLast;
+}
+console.log(removeLastCat())
+
+function  removeFirstCat(){
+    const catRemoveFirst = cats.slice(1)
+    return catRemoveFirst;
+}
+console.log(removeFirstCat())
